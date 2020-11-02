122e22c5d6a8373b6becd94044bfc00e51d3279a
amivaleo <jimmy.scionti@gmail.com>
2020-11-02 18:33:18 +0100

    Added rough support for changing indicator position

--- extension.js
+++ extension.js
@@ -2,6 +2,12 @@ const St = imports.gi.St;
 const Main = imports.ui.main;
 const Util = imports.misc.util;
 const Shell = imports.gi.Shell;
+const Atk = imports.gi.Atk;
+
+const PanelMenu = imports.ui.panelMenu;
+
+
+let position = 'center';		// possible values: 'left', 'center', 'right'
 
 let panelIndicator;
 let minimizedWindows = [];
@@ -9,30 +15,35 @@ let minimizedWindows = [];
 function toggleDesktop() {
 	let metaWorkspace = global.workspace_manager.get_active_workspace();
 	let windows = metaWorkspace.list_windows();
-	
-	if (!Main.overview.visible) {																									// if the user click on the panelIndicator while the overview is open -> do nothing.
+
+	// if the user click on the panelIndicator while the overview is open -> do nothing.
+	if (!Main.overview.visible) {
 		if (allWindowsAreMinimized) {
 			for ( let i = 0; i < windows.length; ++i ) {
 				for (let j = 0; j < minimizedWindows.length; j++) {
-					if (windows[i] == minimizedWindows[j]) {															// if the window was already minimized before...
-						windows.splice(i, 1);																								// ... remove that window from the list of windows that will be uniminimized...
+					// if the window was already minimized before remove that window from the list of windows that will be uniminimized
+					if (windows[i] == minimizedWindows[j]) {
+						windows.splice(i, 1);
 					}
 				}
 			}
-			for ( let i = 0; i < windows.length; ++i ) {															// ... in this loop!
+			// unminimize only those windows there were not minimized before
+			for ( let i = 0; i < windows.length; ++i ) {
 				windows[i].unminimize(global.get_current_time());
 			}
 			minimizedWindows = [];
 		} else {
 			for ( let i = 0; i < windows.length; ++i ) {
-				if (windows[i].minimized) {																							// if the window is already minimized, add it to a separate array...
+				// if the window is already minimized, add it to a separate array...
+				if (windows[i].minimized) {
 					minimizedWindows.push(windows[i]);
+				// ... otherwise minimize that window
 				} else {
-					windows[i].minimize(global.get_current_time());												// ... otherwise minimize that window.
+					windows[i].minimize(global.get_current_time());
 				}
 			}
 		}
-	
+
 		allWindowsAreMinimized = !allWindowsAreMinimized;
 	}
 }
@@ -42,22 +53,21 @@ function init(extensionMeta) {
 }
 
 function enable() {
-	panelIndicator = new St.Bin({
-		style_class: 'panel-button',
-		reactive: true,
-		can_focus: true,
-		track_hover: true});
-		
+	panelIndicator = new PanelMenu.Button(0.0, null, true);
+	panelIndicator.actor.accessible_role = Atk.Role.TOGGLE_BUTTON;
+	
 	let icon = new St.Icon({
 		icon_name: 'user-home-symbolic',
 		style_class: 'system-status-icon'});
-		
-	panelIndicator.set_child(icon);
-	panelIndicator.connect('button-press-event', toggleDesktop);
-	
-	Main.panel._rightBox.insert_child_at_index(panelIndicator, 0);
+
+	panelIndicator.actor.add_actor(icon);
+	panelIndicator.actor.connect('button-press-event', toggleDesktop);
+
+	Main.panel.addToStatusArea("ShowDesktop", panelIndicator, 1, position);
 }
 
 function disable() {
 	Main.panel._rightBox.remove_child(panelIndicator);
+	panelIndicator.destroy();
+	panelIndicator = null;
 }
